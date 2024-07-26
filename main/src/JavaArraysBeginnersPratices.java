6eaa3b7e40614c0b1f8a97699bdfb548854a763b
nadjafov05 <nadjafovp@gmail.com>
2024-07-26 13:19:24 +0400

    1.5.7

--- main/src/JavaArraysBeginnersPratices.java
+++ main/src/JavaArraysBeginnersPratices.java
@@ -70,4 +70,21 @@ public class JavaArraysBeginnersPratices {
 
         System.out.println("B arrayinda`ki komyalanmis melumatlar : "+Arrays.toString(arrB));
     }
+
+    public static void pratices8(){
+        int[] arrInt = {-1,5,11,9};
+        int max = arrInt[0];
+        int min = arrInt[0];
+
+        for (int i = 1;i<arrInt.length;i++){
+            if (arrInt[i] > max){
+                max = arrInt[i];
+            }
+            if (arrInt[i] < min ){
+                min = arrInt[i];
+            }
+        }
+        System.out.println("max deyer :" + max);
+        System.out.println("min deyer :" + min);
+    }
 }
--- main/src/Main.java
+++ main/src/Main.java
@@ -2,14 +2,19 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arrA = {1,2,3,5,6,9,8,7,4};
-        System.out.println("A arrayindaki orginal melumatlar : "+Arrays.toString(arrA));
-        int[] arrB = new int[9];
+        int[] arrInt = {-1,5,11,9};
+        int max = arrInt[0];
+        int min = arrInt[0];
 
-        for (int i=0;i<arrA.length;i++){
-            arrB[i] = arrA[i];
+        for (int i = 1;i<arrInt.length;i++){
+            if (arrInt[i] > max){
+                max = arrInt[i];
+            }
+            if (arrInt[i] < min ){
+                min = arrInt[i];
+            }
         }
-
-        System.out.println("B arrayinda`ki komyalanmis melumatlar : "+Arrays.toString(arrB));
+        System.out.println("max deyer :" + max);
+        System.out.println("min deyer :" + min);
     }
 }
